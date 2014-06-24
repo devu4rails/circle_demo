9201b7d70d8d0337222bf389238fda514209345e
devudilip <devu.dilip@gmail.com>
2014-06-24 16:18:14 +0530

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CircleDemo::Application.config.secret_key_base = '51d03a1514093104dc14919ab368eb1658ff388533755ef6df5d04f55c8c59ff1ee775b0c57902f3e088e4373a766ff51b9b67655403db447e1f9486c09fe91d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CircleDemo::Application.config.session_store :cookie_store, key: '_circle_demo_session'
